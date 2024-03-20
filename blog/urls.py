7aeff250fdec73d1e3de339a7b29c5088679bd3c
Anton Tkachuk <tkacuk2291@gmail.com>
2024-03-21 00:38:49 +0200

    implement post_detail page

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from blog.views import index,about_us,contact_us,author , PostListView
+from blog.views import index, about_us, contact_us, author, PostListView, PostDetailView
 
 urlpatterns = [
-    path("new/", index, name="index-task"),
+    path("home/", index, name="home-page"),
     path('about-us/', about_us, name='about-us'),
     path('contact-us/', contact_us, name='contact-us'),
     path('author/', author, name='author'),
-    path("posts/", PostListView.as_view(), name="index-post")
+    path("posts/", PostListView.as_view(), name="post-list"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail")
 ]
 
 app_name = "blog"
