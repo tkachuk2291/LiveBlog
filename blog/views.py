d297c663c99849c1c76f698a572c63f919d625cf
Anton Tkachuk <tkacuk2291@gmail.com>
2024-03-21 16:07:53 +0200

    add new view logout

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
 from django.views import generic
 
-from blog.models import Post
+from blog.forms import UserLoginForm
+from blog.models import Post, User
 
 
 def index(request):
@@ -26,7 +29,31 @@ class PostListView(generic.ListView):
     template_name = "blog-templates/posts/post_list.html"
 
 
+class UserLoginView(LoginView):
+    template_name = 'blog-templates/accounts/sign-in.html'
+    form_class = UserLoginForm
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("blog:home-page")
+
+
 class PostDetailView(generic.DetailView):
     model = Post
-    context_object_name = "post"
     template_name = "blog-templates/posts/post_detail.html"
+    context_object_name = "post_detail"
+
+    def get_context_data(self, **kwargs):
+        context_data = super(PostDetailView, self).get_context_data(**kwargs)
+        try:
+            username = User.objects.first()
+            post = self.get_object()
+            user = User.objects.get(username=username.username)
+            posts = user.owner_posts.all()
+            context_data['posts'] = posts
+            context_data['all_posts'] = posts.exclude(pk=post.pk)
+        except User.DoesNotExist:
+            pass
+
+        return context_data
