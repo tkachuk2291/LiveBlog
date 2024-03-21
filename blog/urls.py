26cb0a22fece6ccba6ed590fa222057cc3fa4591
Anton Tkachuk <tkacuk2291@gmail.com>
2024-03-21 16:01:39 +0200

    implement login page with authentification

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-from blog.views import index, about_us, contact_us, author, PostListView, PostDetailView
+from django.urls import path, include
+from blog.views import index, about_us, contact_us, author, PostListView, PostDetailView , UserLoginView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("home/", index, name="home-page"),
@@ -7,7 +8,13 @@ urlpatterns = [
     path('contact-us/', contact_us, name='contact-us'),
     path('author/', author, name='author'),
     path("posts/", PostListView.as_view(), name="post-list"),
-    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail")
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("accounts/login/", UserLoginView.as_view(), name="login")
+    # path("accounts/login/", auth_views.LoginView.as_view(template_name="blog-templates/accounts/sign-in.html"),
+    #      name="login"),
+    # path("accounts/logout/", auth_views.LogoutView.as_view(template_name="blog-templates/accounts/sign-up.html"),
+    #      name="logout")
+
 ]
 
 app_name = "blog"
