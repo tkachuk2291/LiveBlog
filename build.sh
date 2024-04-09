f5541f2e28a686837461893ed955aed2132368c2
Anton Tkachuk <132574548+tkachuk2291@users.noreply.github.com>
2024-04-09 21:14:13 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,5 +9,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py makemigrations blog
 python manage.py migrate
